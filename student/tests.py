0b6ad0ef2695b97f27506fa1b4d811f509bc4dda
hzkljy <850316314@qq.com>
2019-03-16 21:39:37 +0800

    新建python2.7学生管理系统后台

---
+++ student/tests.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase, Client
+
+from .models import Student
+
+# Create your tests here.
---
+++ student_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hzkljy/code/student/student_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
